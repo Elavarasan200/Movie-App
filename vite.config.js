26d09c18388c8d3d377c0536d57da5ab965e0751
Elavarasan M <127094940+Elavarasan200@users.noreply.github.com>
2026-08-13 19:13:45 +0530

    Set base path for Movie-App in vite.config.js
    Add base path for deployment in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Movie-App/', // <- add this line
   plugins: [react()],
   server: {
     port: 5173,
